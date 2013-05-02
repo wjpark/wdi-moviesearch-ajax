77c61be6e91d9744f17ee94bfd42f7763bf00d17
Woo Jin Park <woojinp@gmail.com>
2013-05-01 21:54:55 -0700

    completed search functionality

--- movies.js
+++ movies.js
@@ -20,13 +20,13 @@
 
 
 $(document).ready(function(){
-  $("input").keyup("click", function(){
+  $("input").bind("keyup click", function(){
     var entry = $("input").val();
     $.ajax({
       url: "http://www.omdbapi.com/?s=" + entry,
       method: 'get',
       dataType: "jsonp",
-      success:function(movies){
+      success: function(movies){
         results = movies.Search;
         $('#results').html('');
         for (var i = 0; i < results.length; i += 1) {
@@ -35,12 +35,29 @@ $(document).ready(function(){
           var title = movie.Title;
           var year = movie.Year;
           var id = movie.imdbID;
-          $("<p data-id=\"" + id + "\">" + title + "(" + year + ")" + "</p>").appendTo("#results");
+          $("<p data-id=\"" + id + "\">" + title + " (" + year + ")" + "</p>").appendTo("#results");
         }
+        $("#results").delegate("p", "click",function(){
+          //$(this).css("background-color","gray");
+          var movie = $(this);
+          var movie_id = $(movie).attr('data-id');
+          //console.log(movie_id);
+          $.ajax({
+            url: "http://www.omdbapi.com/?i=" + movie_id,
+            method: "get",
+            dataType: "jsonp",
+            success: function(movie){
+              var title = movie.Title;
+              var year = movie.Year;
+              var plot = movie.Plot;
+              var actors = movie.Actors;
+              var poster =movie.Poster;
+              console.log(poster);
+              $("<img src='" + poster + "'>").appendTo("#movie_detail");
+            }
+          });
+        });
       }
     });
   });
 });
-
-
-
